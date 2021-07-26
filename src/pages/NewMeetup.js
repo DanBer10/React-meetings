64d1ca8463d43e9c52cf7c150a75eeb7484b4927
dannebernhardt199610 <danielbernhardt1996@outlook.com>
2021-07-26 17:02:35 +0200

    a

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import { useHistory } from 'react-router-dom'
+import NewMeetupForm from '../Components/meetups/NewMeetupForm'
 
 export default function NewMeetup() {
+    const history = useHistory();
+
+    function addMeetupHandler(meetupData) {
+        fetch('https://react-todos-8bde1-default-rtdb.firebaseio.com/meetups.json',
+         {
+            method: 'POST',
+            body: JSON.stringify(meetupData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        ).then(() => {
+            history.replace('/');
+        })
+    }
     return (
         <div>
-            new meetup page
+         <h1>Add New Meetup</h1>
+         <NewMeetupForm onAddMeetup={addMeetupHandler} />
         </div>
     )
 }
